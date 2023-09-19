cca6fbc025b6bb700bc1cd1d453544f441ef0dd4
postfm <postfm@yandex.ru>
2023-09-19 12:16:43 +0300

    Добавит стилизацию и адаптивность

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        'layout': 'auto 1fr auto'
+      },
+      colors: {
+        'logo-main': '#9b0404'
+      },
+      fontFamily: {
+        'main': ['Roboto']
+      }
+    },
+  },
+  plugins: [],
+}
+
